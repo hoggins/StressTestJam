e6ee8c305e3e57112459bc83810f6131bc2795e6
m.gukov <m.gukov@plarium.com>
2018-06-17 02:51:49 +0300

    allow camera to flow by Y

--- Unity/Assets/Scripts/CameraController.cs
+++ Unity/Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
   public GameObject Target;
   public float Speed;
   public Vector3 Offset;
+  public Vector2 OffsetYClamp;
   public static CameraController I;
   public Camera Native;
 
@@ -25,7 +26,10 @@ public class CameraController : MonoBehaviour
     if(Target == null)
       return;
 
-    transform.position = new Vector3(Offset.x, Offset.y , Mathf.Lerp(transform.position.z, Target.transform.position.z + Offset.z, Time.deltaTime*Speed));
+		var lerpZ = Mathf.Lerp(transform.position.z, Target.transform.position.z + Offset.z, Time.deltaTime*Speed);
+		var lerpY = Mathf.Lerp(transform.position.y, Target.transform.position.y /*+ Offset.y*/, Time.deltaTime*(Speed*0.3f));
+		lerpY = Mathf.Clamp(lerpY, OffsetYClamp.x, OffsetYClamp.y);
+		transform.position = new Vector3(Offset.x, lerpY , lerpZ);
 
 	  //transform.position = Vector3.Lerp(transform.position, Target.transform.position + Offset, Time.deltaTime);
 	}

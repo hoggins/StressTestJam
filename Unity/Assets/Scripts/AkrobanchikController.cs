73b62f868bbe126edec22734951d3cad5124777c
Serhii Kryvych <s.kryvych@plarium.com>
2018-06-16 13:56:00 +0300

    Akrobanchiks control

---
+++ Unity/Assets/Scripts/AkrobanchikController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AkrobanchikController : MonoBehaviour
+{
+  public float FollowSpeed;
+  private int _index;
+  public float XOffset = 2;
+  public float YOffset = 2f;
+  public float ZOffset = 0.1f;
+
+  public Vector3 _positionOffset;
+  private float _elapsed;
+  private float _elapsed2;
+
+  void Start ()
+  {
+    GetPositionOffset();
+    StartCoroutine(PositionOffsetChanger());
+    _elapsed = Random.Range(-Mathf.PI, Mathf.PI);
+    _elapsed2 = Random.Range(-Mathf.PI, Mathf.PI);
+  }
+
+  private void GetPositionOffset()
+  {
+    _positionOffset = new Vector3(Random.Range(-XOffset, XOffset),
+      Random.Range(-YOffset, YOffset),
+      Random.Range(-ZOffset, ZOffset));
+  }
+
+  void Update ()
+  {
+    _elapsed += Time.deltaTime;
+    _elapsed2 += Time.deltaTime;
+
+    _positionOffset.y += Mathf.Sin(_elapsed)*Time.deltaTime*0.25f;
+    _positionOffset.x += Mathf.Cos(_elapsed2)*Time.deltaTime*0.5f;
+
+	  transform.position = Vector3.Lerp(transform.position,
+	    PlayerControl.I.transform.position + _positionOffset,
+	    Time.deltaTime*Mathf.Sqrt(_index)*FollowSpeed);
+	}
+
+  public void SetIndex(int index)
+  {
+    _index = index;
+  }
+
+  private IEnumerator PositionOffsetChanger()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(Random.Range(1f, 2f));
+    //  GetPositionOffset();
+    }
+  }
+}
--- Unity/Assets/Scripts/PlayerControl.cs
+++ Unity/Assets/Scripts/PlayerControl.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+  public static PlayerControl I;
+
   public float ForwardMaxSpeed = 10;
   public float ForwardAcceleration = 1;
 
@@ -9,18 +12,47 @@ public class PlayerControl : MonoBehaviour
   public float DirectionMaxSpeed = 5f;
 
   public float SlowDown = 1f;
+  public float FallSpeed = 0.25f;
 
   public Vector3 Velocity;
+
+  public int AkrobanchiksCount = 30;
+
+
+  public GameObject AkrobanchikPrefab;
+  private List<AkrobanchikController> _akrobanchiks;
+
   private Vector2 _input;
+
+  void Awake()
+  {
+    I = this;
+  }
 
   void Start()
   {
+    SpawnAkrobanchiks();
+  }
 
+  private void SpawnAkrobanchiks()
+  {
+    _akrobanchiks = new List<AkrobanchikController>();
+    for (int i = 0; i < AkrobanchiksCount; i++)
+    {
+      var go = Instantiate(AkrobanchikPrefab, transform.position, Quaternion.identity);
+      var ac = go.GetComponent<AkrobanchikController>();
+      ac.SetIndex(i);
+    }
   }
 
   public void SetInput(Vector2 input)
   {
     _input = input;
+
+    if (_input.y < 0.01f)
+    {
+      _input.y = -FallSpeed;
+    }
   }
 
   void Update()
@@ -38,7 +70,7 @@ public class PlayerControl : MonoBehaviour
 
     Velocity.z = Mathf.Min(ForwardMaxSpeed, Velocity.z);
     Velocity.y = Mathf.Sign(Velocity.y) * Mathf.Min(DirectionMaxSpeed, Mathf.Abs(Velocity.y));
-    Velocity.z = Mathf.Sign(Velocity.z) * Mathf.Min(DirectionMaxSpeed, Mathf.Abs(Velocity.z));
+    Velocity.x = Mathf.Sign(Velocity.x) * Mathf.Min(DirectionMaxSpeed, Mathf.Abs(Velocity.x));
 
     transform.position += Velocity*Time.deltaTime;
   }

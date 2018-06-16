90ef95c6abb4d172412e7d66a34443995da114fe
m.gukov <m.gukov@plarium.com>
2018-06-16 15:03:23 +0300

    add mic input

---
+++ Unity/Assets/Scripts/Mic/MicInputAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MicInputAdapter : MonoBehaviour
+    {
+
+        public static float FinalLevel;
+
+        public float SmoothPerc = 0.1f;
+        public float Fade = 0.01f;
+        
+        private const int Rate = 5;
+        
+        private void Update()
+        {
+            var level = MicInput2.MicLoudness;
+
+            FinalLevel = (FinalLevel + level - Fade) * SmoothPerc;
+
+            FinalLevel = Mathf.Clamp01(FinalLevel);
+            
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Mic/MicLevelIndicator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MicLevelIndicator : MonoBehaviour
+    {
+        private GameObject _indic;
+
+        private void OnEnable()
+        {
+            _indic = GameObject.Find("Cube");
+        }
+
+        private void Update()
+        {
+            var level = MicInputAdapter.FinalLevel;
+            _indic.transform.localScale = new Vector3(1,level,1);
+        }
+    }
+}

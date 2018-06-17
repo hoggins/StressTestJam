4ff2e0c4ff705a40ae06420cb16d9e005cb45bbd
m.gukov <m.gukov@plarium.com>
2018-06-17 10:44:39 +0300

    voice suppression on mic

--- Unity/Assets/Scripts/Mic/MicInputAdapter.cs
+++ Unity/Assets/Scripts/Mic/MicInputAdapter.cs
@@ -11,8 +11,11 @@ namespace DefaultNamespace
 
         public float DecVal = -0.1f;
         public float IncVal = 0.5f;
-        
 
+        public float MedianStill = 1;
+        public float MedianActive = 0.7f;
+        public float RaiseTimeSec = 0.5f;
+        
         private float _lastLevel;
         
         private void Update()
@@ -27,7 +30,14 @@ namespace DefaultNamespace
             if (FinalLevel < 0.3)
                 FinalLevel = 0;
             else
-            FinalLevel = Mathf.Clamp01(FinalLevel);
+                FinalLevel = Mathf.Clamp01(FinalLevel);
+
+            if (FinalLevel > 0.3f)
+                MicInput2.Inctance.MedianPercent = Mathf.Lerp(MicInput2.Inctance.MedianPercent, MedianActive, Time.deltaTime / RaiseTimeSec);
+            else
+                MicInput2.Inctance.MedianPercent = Mathf.Lerp(MedianStill, MicInput2.Inctance.MedianPercent, Time.deltaTime / RaiseTimeSec);
+            
+            MicInput2.Inctance.MedianPercent = Mathf.Clamp(MicInput2.Inctance.MedianPercent, MedianActive, MedianStill);
         }
     }
 }

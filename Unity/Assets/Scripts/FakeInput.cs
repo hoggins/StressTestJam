7fe9484c0b03bbb88d4ef387cea958965eb0831f
m.gukov <m.gukov@plarium.com>
2018-06-17 14:13:47 +0300

    implement fake input

---
+++ Unity/Assets/Scripts/FakeInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class FakeInput : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+    {
+        public static bool IsActive;
+        public static float Value;
+        
+        private Image _indic;
+
+        private void Awake()
+        {
+            _indic = GameObject.Find("FakeIndic").GetComponent<Image>();
+            _indic.enabled = false;
+        }
+
+        public void DoActivateFake()
+        {
+            IsActive = !IsActive;
+                _indic.enabled = IsActive;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Value = 0;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Value = 1;
+        }
+    }
+}
